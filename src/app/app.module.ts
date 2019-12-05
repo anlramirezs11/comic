625b2e89cca145ecfb3f6a0b2c0e07d293bfa17d
Andrea <anlramirezs11@gmail.com>
2019-12-05 11:25:49 -0500

    Se habilitan las rutas el nav.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 //Rutas
-import { APP_ROUTING } from './app.routes'
+import { APP_ROUTING } from './app.routes';
 
 //Componenetes
 import { AppComponent } from './app.component';
